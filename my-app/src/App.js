a42615ab38ba7435137aa53bee829d919986e951
Tam NTM <tamntm39@gmail.com>
2024-12-17 22:43:35 +0700

    Tictactoe Game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import YoutubeList from '../src/components/youtube/YoutubeList';
+import Toggle from './components/toggle/Toggle';
+import Counter from "./components/counter/Counter";
+import Game from "./components/tictactoe/Game";
+
 function App() {
   return (
    <div>
-    <YoutubeList></YoutubeList>
+    {/* <YoutubeList></YoutubeList> */}
+    {/* <Toggle></Toggle>
+    <Counter></Counter> */}
+   <Game></Game>
    </div>
   );
 }
